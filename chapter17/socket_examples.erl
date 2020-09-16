dd47109a9f6f43c73286642a10f7629f451c393d
Shishir Pandey <shishir.py@gmail.com>
2020-09-16 15:49:59 -0400

    more socket examples

--- chapter17/socket_examples.erl
+++ chapter17/socket_examples.erl
@@ -1,5 +1,7 @@
 -module(socket_examples).
--export([nano_get_url/0]).
+-export([nano_get_url/0, start_nano_server/0, loop/1,
+         nano_client_eval/1, start_seq_server/0,
+         start_parallel_server/0]).
 
 nano_get_url() ->
     nano_get_url("www.wikipedia.org").
@@ -16,3 +18,66 @@ receive_data(Socket, SoFar) ->
         {tcp_closed, Socket} ->
             list_to_binary(lists:reverse(SoFar))
         end.
+
+
+start_nano_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    {ok, Socket} = gen_tcp:accept(Listen),
+    gen_tcp:close(Listen),
+    loop(Socket).
+
+
+loop(Socket)->
+    receive
+        {tcp, Socket, Bin} ->
+            io:format("Server received binary = ~p~n", [Bin]),
+            Str = binary_to_term(Bin),
+            io:format("Server (unpacked) ~p~n", [Str]),
+            Reply = Str,
+            io:format("Server replying  = ~p~n", [Reply]),
+            gen_tcp:send(Socket, term_to_binary(Reply)),
+            loop(Socket);
+        {tcp_closed, Socket} ->
+            io:format("Server socket closed ~n")
+    end.
+
+nano_client_eval(Str) ->
+    {ok, Socket} = gen_tcp:connect("localhost", 2345, [binary, {packet, 4}]),
+    ok = gen_tcp:send(Socket, term_to_binary(Str)),
+    receive
+        {tcp, Socket, Bin} ->
+            io:format("Client received binary = ~p~n", [Bin]),
+            Val = binary_to_term(Bin),
+            io:format("Client result = ~p~n", [Val]),
+            gen_tcp:close(Socket)
+    end.
+
+
+
+start_seq_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    seq_loop(Listen).
+
+
+seq_loop(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    loop(Socket),
+    seq_loop(Listen).
+
+
+
+start_parallel_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, treu}]),
+    spawn(fun() -> par_connect(Listen) end).
+
+
+par_connect(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    spawn(fun() -> par_connect(Listen) end),
+    loop(Socket).
